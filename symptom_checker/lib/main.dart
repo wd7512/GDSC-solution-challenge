b9cfcb5220bfae169602c5277975b3db398de8b2
William Dennis <wwdennis.home@gmail.com>
2024-02-05 22:17:27 +0000

    adding relative coordinate system

--- symptom_checker/lib/main.dart
+++ symptom_checker/lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:symptom_checker/components/category.dart';
+import 'package:symptom_checker/components/selected_symptoms.dart';
+import 'package:symptom_checker/models/data_store.dart';
+import 'dart:math';
+import 'package:symptom_checker/models/selected_symptoms_store.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final GlobalKey _imageKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,15 +22,24 @@ class MyApp extends StatelessWidget {
           children: [
             Image.asset(
               "assets/StickMan.jpeg",
-              fit: BoxFit.cover,
+              key: _imageKey,
+              fit: BoxFit.contain,
               width: double.infinity,
               height: double.infinity,
             ),
             GestureDetector(
               onTapDown: (TapDownDetails details) {
-                double x = details.globalPosition.dx;
-                double y = details.globalPosition.dy;
-                print('Button pressed at coordinates: x=$x, y=$y');
+                double x = details.globalPosition.dx / MediaQuery.of(context).size.width;
+                double y = details.globalPosition.dy / MediaQuery.of(context).size.height;
+                Point loc = Point(x,y);
+                String part = LocationStore().findClosestPoint(loc);
+                print('Button pressed at coordinates: x=$x, y=$y, part=$part');
+                // goes to 
+                // Ensure the context used here has a Navigator
+                //Navigator.push(
+                //  context,
+                //  MaterialPageRoute(builder: (context) => AnotherPage()),
+                //);
               },
             ),
           ],
--- symptom_checker/lib/models/data_store.dart
+++ symptom_checker/lib/models/data_store.dart
@@ -2,6 +2,11 @@
 /// Class encapsulating all categories, bodyparts, and symptoms.
 /// - currently hardcoded, but can be changed to an ORM
 /// 
+/// 
+/// 
+/// 
+import 'dart:math';
+
 class DataStore {
   
   // map of data[category][bodypart]:symptoms
@@ -60,4 +65,41 @@ class DataStore {
     return data[category]![bodypart] ?? [];
   }
 
+}
+
+class LocationStore {
+  // Your existing map
+  Map<Point, String> data = {
+    Point(0.5, 0.2): 'head',
+    Point(0.5, 0.5): 'torso',
+    Point(0.65, 0.4): 'arms',
+    Point(0.35, 0.4): 'arms',
+    Point(0.5, 0.75): 'legs',
+  };
+
+  List<Point> getAllPoints() {
+    return data.keys.toList();
+  }
+
+  String findClosestPoint(Point loc) {
+    double minDistance = double.infinity;
+    Point closestPoint = Point(0, 0);
+
+    for (Point point in data.keys) {
+      double distance = calculateDistance(loc, point);
+
+      if (distance < minDistance) {
+        minDistance = distance;
+        closestPoint = point;
+      }
+    }
+
+    return data[closestPoint]!;
+  }
+
+  double calculateDistance(Point a, Point b) {
+    double dx = (a.x.toDouble() - b.x.toDouble());
+    double dy = (a.y.toDouble() - b.y.toDouble());
+    return sqrt(dx * dx + dy * dy);
+  }
 }
