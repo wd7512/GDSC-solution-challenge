90c1a07c82fbae58c4cc4168bdb50d2e359b1c26
osbson <orebenson@hotmail.com>
2024-02-06 16:06:33 +0000

    config

---
+++ symptom_checker/lib/components/body_selector.dart
@@ -0,0 +1,37 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:symptom_checker/components/category.dart';
+import 'package:symptom_checker/models/data_store.dart';
+
+class BodySelectorStack extends StatelessWidget {
+  const BodySelectorStack({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        GestureDetector(
+          onTapDown: (TapDownDetails details) {
+            double x = details.globalPosition.dx / MediaQuery.of(context).size.width;
+            double y = details.globalPosition.dy / MediaQuery.of(context).size.height;
+            Point loc = Point(x,y);
+            String part = LocationStore().findClosestPoint(loc);
+            print('Button pressed at coordinates: x=$x, y=$y, part=$part');
+            // goes to 
+            // Ensure the context used here has a Navigator
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => CategoryPage(category: part)),
+            );
+          },
+          child: Image.asset(
+            "assets/CartoonPerson.jpeg",
+            fit: BoxFit.contain,
+          ),
+
+        ),
+      ],
+    );
+  }
+}
