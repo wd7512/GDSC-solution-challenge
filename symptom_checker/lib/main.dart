34ade49c2324020712b88b7ad5665ab4e3d20a35
osbson <orebenson@hotmail.com>
2024-02-06 16:06:17 +0000

    Fixed and refactored body selector

--- symptom_checker/lib/main.dart
+++ symptom_checker/lib/main.dart
@@ -1,53 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:symptom_checker/components/category.dart';
+import 'package:symptom_checker/components/body_selector.dart';
+// import 'package:symptom_checker/components/category.dart';
 import 'package:symptom_checker/components/selected_symptoms.dart';
-import 'package:symptom_checker/models/data_store.dart';
-import 'dart:math';
+// import 'package:symptom_checker/models/data_store.dart';
 import 'package:symptom_checker/models/selected_symptoms_store.dart';
 
 void main() {
- runApp(MaterialApp(
-   title: 'Symptom Checker App',
-   home: MyApp(),
- ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final GlobalKey _imageKey = GlobalKey();
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Symptom Checker")),
-        body: Stack(
-          children: [
-            Image.asset(
-              "assets/CartoonPerson.jpeg",
-              key: _imageKey,
-              fit: BoxFit.contain,
-              width: double.infinity,
-              height: double.infinity,
-            ),
-            GestureDetector(
-              onTapDown: (TapDownDetails details) {
-                double x = details.globalPosition.dx / MediaQuery.of(context).size.width;
-                double y = details.globalPosition.dy / MediaQuery.of(context).size.height;
-                Point loc = Point(x,y);
-                String part = LocationStore().findClosestPoint(loc);
-                print('Button pressed at coordinates: x=$x, y=$y, part=$part');
-                // goes to 
-                // Ensure the context used here has a Navigator
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CategoryPage(category: part)),
-                );
-              },
-            ),
-          ],
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => SelectedSymptoms()),
+      ],
+      child: MaterialApp(
+        title: 'Symptom Tracker',
+        home: const HomePage(),
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 147, 164, 202)),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Color.fromARGB(255, 147, 164, 202),
+          ),
         ),
       ),
     );
   }
 }
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Symptom Checker'),
+      ),
+      body: const Center(
+        child: Padding(
+          padding: EdgeInsets.only(top: 16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // for (String category in DataStore().getAllCategories()) CategoryButton(category: category),
+                AllSymptomsButton(),
+                BodySelectorStack(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
