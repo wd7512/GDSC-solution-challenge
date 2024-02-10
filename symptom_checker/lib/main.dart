efb9014a3a72cf0a045b37361d42d84dba66477d
osbson <orebenson@hotmail.com>
2024-02-10 11:15:54 +0000

    Reverted to button selectors

--- symptom_checker/lib/main.dart
+++ symptom_checker/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:symptom_checker/components/body_selector.dart';
+import 'package:symptom_checker/components/category.dart';
 // import 'package:symptom_checker/components/category.dart';
 import 'package:symptom_checker/components/selected_symptoms.dart';
+import 'package:symptom_checker/models/data_store.dart';
 // import 'package:symptom_checker/models/data_store.dart';
 import 'package:symptom_checker/models/selected_symptoms_store.dart';
 
@@ -43,17 +45,15 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Symptom Checker'),
       ),
-      body: const Center(
+      body: Center(
         child: Padding(
-          padding: EdgeInsets.only(top: 16.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                // for (String category in DataStore().getAllCategories()) CategoryButton(category: category),
-                AllSymptomsButton(),
-                BodySelectorStack(),
-              ],
-            ),
+          padding: const EdgeInsets.only(top: 16.0),
+          child: Column(
+            children: [
+              for (String category in DataStore().getAllCategories()) CategoryButton(category: category),
+              const AllSymptomsButton(),
+              // BodySelectorStack(),
+            ],
           ),
         ),
       ),
