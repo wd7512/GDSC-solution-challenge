2e31b7b175163dbe4b9d8ee69e5024709829b6d1
Lamb Chen <lambjchen@gmail.com>
2024-02-14 16:39:48 +0000

    Got basic PDF convertor working

--- symptom_checker/lib/utility/footer_util.dart
+++ symptom_checker/lib/utility/footer_util.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:symptom_checker/components/selected_symptoms_components.dart';
+import 'package:symptom_checker/models/selected_symptoms_store.dart';
 import 'package:symptom_checker/utility/button_util.dart';
 
 // Reusable component for footer
 
 class Footer extends StatelessWidget {
-  const Footer({super.key});
+  const Footer({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.center, // Center the buttons horizontally
+    final selectedSymptoms = Provider.of<SelectedSymptoms>(context);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        HomeButton(), 
-        AllSymptomsButton(),
+        const HomeButton(),
+        const AllSymptomsButton(),
+        ElevatedButton(
+          onPressed: () async {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => PdfPreviewPage(
+                  selectedSymptoms: selectedSymptoms.selectedSymptoms,
+                ),
+              ),
+            );
+            // rootBundle.
+          },
+          child: const Text("Generate PDF"),
+        ),
       ],
     );
   }
